6dc5d4471fd213df0f0123bf658008c0674fd4f2
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-12-02 17:57:33 -0800

    Initial Commit

---
+++ RickAndMortyConsumerApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyConsumerApp Watch App
+//
+//  Created by francisco eduardo aramburo reyes on 02/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyConsumerApp Watch App/RickAndMortyConsumerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyConsumerAppApp.swift
+//  RickAndMortyConsumerApp Watch App
+//
+//  Created by francisco eduardo aramburo reyes on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyConsumerApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
